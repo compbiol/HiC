a6841649ad6b278a1b593fc06299cdf2438bee2d
aganezov <aganezov@gwu.edu>
2016-09-19 14:39:03 -0400

    dummy template for genome wide hic export

--- processing/juicer/hic_cell_line.sh
+++ processing/juicer/hic_cell_line.sh
@@ -12,12 +12,10 @@ then
 
         * -h : show this help message and exit
         * (1) cell_line_name: string representing a cell line to work with. Available cell lines: ${!amazon_sources[@]}
-        * (2) data: a type of data, one wishes to export. Available options: [observed|oe|pearson|norm|expected], where last two produce vectors.
-        * (3) normalization: type of normalization, to be applied to the exported data. Available options: [NONE/VC/VC_SQRT/KR].
-        * (4) resolution: a unit of resolution. Available options: [BP|FRAG]
-        * (5) bin_size: a size of the matrix entry in the resolution unit specified above.
-        * (6) output
-        * (7) juicebox_tools_path: a path to juicebox_tools.jar file
+        * (2) normalization: type of normalization, to be applied to the exported data. Available options: [NONE/VC/VC_SQRT/KR].
+        * (3) bin_size: a size of the matrix entry in the resolution unit specified above.
+        * (4) output
+        * (5) juicebox_tools_path: a path to juicebox_tools.jar file
     "
     exit 0
 fi
@@ -29,5 +27,27 @@ RABBIT_CHROMOSOMES=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14"
 MACAQUE_CHROMOSOMES=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "X")
 
 declare -A cell_lines_chromosomes=(
-    [""]
+    ["rao14_gm12878"]=HUMAN_CHROMOSOMES
+    ["rao14_imr90"]=HUMAN_CHROMOSOMES
+    ["rao14_hmec"]=HUMAN_CHROMOSOMES
+    ["rao14_nhek"]=HUMAN_CHROMOSOMES
+    ["rao14_k562"]=HUMAN_CHROMOSOMES
+    ["rao14_kbm7"]=HUMAN_CHROMOSOMES
+    ["rao14_huvec"]=HUMAN_CHROMOSOMES
+    ["rao14_huvec"]=HUMAN_CHROMOSOMES
+    ["rao14_chx12"]=MOUSE_CHROMOSOMES
+    ["rudan15_dog1"]=DOG_CHROMOSOMES
+    ["rudan15_dog2"]=DOG_CHROMOSOMES
+    ["rudan15_mouse1"]=MOUSE_CHROMOSOMES
+    ["rudan15_mouse2"]=MOUSE_CHROMOSOMES
+    ["rudan15_macaque1"]=MACAQUE_CHROMOSOMES
+    ["rudan15_macaque2"]=MACAQUE_CHROMOSOMES
+    ["rudan15_rabbit1"]=RABBIT_CHROMOSOMES
+    ["rudan15_rabbit2"]=RABBIT_CHROMOSOMES
 )
+
+for chr1 in ${cell_lines_chromosomes[$1]}; do
+    for chr2 in ${cell_lines_chromosomes[$1]}; do
+        ./export_hic $1 ${chr1} ${chr2} observed $2 BP $3
+    done
+done
