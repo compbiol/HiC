6d001443a990cc9fba307a61f7853404da651a2b
aganezov <aganezov@gwu.edu>
2016-09-20 10:43:10 -0400

    update

--- processing/hic/frag_matrix.py
+++ processing/hic/frag_matrix.py
@@ -1,25 +1,52 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
+
 import argparse
+import csv
+import logging
+import os
 import sys
 from collections import defaultdict
 
-import pandas as pd
 import numpy as np
-import os
+import pandas as pd
 
 
-def get_hic_data(genome, source_folder):
-    if source_folder is None:
-        source_folder = genome
-    file_names = [file_name for file_name in os.listdir(genome) if file_name.startswith(genome)]
+class Fragment(object):
+    __slots__ = ["name", "start", "end", "chromosome"]
 
+    def __init__(self, name, start, end, chromosome):
+        self.name = name
+        self.start = start
+        self.end = end
+        self.chromosome = chromosome
+
+
+def load_hic_data(genome, chr1, chr2, resolution, correction, source_folder):
+    file_basename = "{cell_line}_{chr1}_{chr2}_{resolution}_{correction}.txt".format(
+        cell_line=genome, chr1=chr1, chr2=chr2, resolution=resolution, correction=correction)
+    file_name = os.path.join(source_folder, file_basename)
+    logger.info("Loading data from {file_name} into memory".format(file_name=os.path.basename(file_name)))
     result = defaultdict(dict)
+    reader = csv.reader(file_name, delimiter='\t', quotechar='|')
+    for row in reader:
+        row_id, column_id, value = int(row[0]), int(row[1]), float(row[2])
+        if row_id < column_id:
+            result[row_id][column_id] = value
+        else:
+            result[column_id][row_id] = value
     return result
 
 
 def get_fragments(fragments_file):
-    result = {}
+    logger.info("Loading fragments data from {file_name}".format(file_name=os.path.basename(fragments_file)))
+    result = defaultdict(list)
+    reader = csv.reader(fragments_file, delimiter="\t")
+    for row in reader:
+        name, start, end, chromosome = row[0], int(row[1]), int(row[2]), row[3]
+        result[chromosome].append(Fragment(name=name, start=start, end=end, chromosome=chromosome))
+    total = sum(lambda fr_list: len(fr_list), (result[chromosome] for chromosome in result))
+    logger.info("Obtained a total of {fr_cnt} fragments over {chr_cnt} chromosomes".format(fr_cnt=total, chr_cnt=len(result)))
     return result
 
 
@@ -36,7 +63,11 @@ if __name__ == "__main__":
     parser.add_argument("--measure", choices=["outer", "inner", "fraction"], default="outer")
     parser.add_argument("-o", "--output", default=sys.stdout, type=argparse.FileType("wt"))
     args = parser.parse_args()
-    data = get_hic_data(genome=args.genome, source_folder=args.hic_source)
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        datefmt='%m-%d %H:%M:%S')
+    logger = logging.getLogger("frag_matrix")
+    logger.setLevel(logging.DEBUG)
     fragments = get_fragments(fragments_file=args.fragments)
     contacts = pd.DataFrame(np.nan, index=fragments.keys(), columns=fragments.keys())
     for fragment1 in sorted(fragments.keys()):
